a13580ac5ac1bc62ef8063c697a7b75ae0fa6793
DuncanKeller <keller.duncan@gmail.com>
2012-06-27 12:00:24 -0700

    sideBar, titleBar, and index stub

---
+++ index.php
@@ -0,0 +1,16 @@
+<head>
+
+</head>
+<body>
+
+	<div class = "page">
+		<div class = "sideBar">
+			<?php include "sideBar.php"; ?>
+		</div>
+
+		<div class = "titleBar">
+			<?php include "titleBar.php"; ?>
+		</div>
+	</div>
+
+</body>
---
+++ sideBar.php
@@ -0,0 +1,16 @@
+<div class = "button" >
+	<a href = "#">About</a>
+</div>
+<div class = "button" >
+	<a href = "#">Games</a>
+</div>
+<div class = "button" >
+	<a href = "#">Resume</a>
+</div>
+<div class = "button" >
+	<a href = "#">Achivements</a>
+</div>
+<div class = "button" >
+	<a href = "#">Github</a>
+</div>
+
