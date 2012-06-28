3808ced4dc77ea0f1e7c78f1e100a778c12c0a78
DuncanKeller <keller.duncan@gmail.com>
2012-06-28 11:03:05 -0700

    added a library linking file, made a name change to topBar (changed to titleBar)

---
+++ libraries.php
@@ -0,0 +1,4 @@
+<SCRIPT type="text/javascript" src="lib/jquery.js" ></SCRIPT>
+<SCRIPT type="text/javascript" src="lib/jquery.spritely-0.6.js" ></SCRIPT>
+<SCRIPT type="text/javascript" src="lib/animator.min.js" ></SCRIPT>
+<SCRIPT type="text/javascript" src="lib/jquery.processing.js" ></SCRIPT>
